b97a6b759a9d21db91dd48afc2d6371a39271e9d
Randall Alquicer <randall.bposeats@gmail.com>
2024-12-12 06:48:35 +0800

    adds some functions for the rest

--- binomial_heap.cpp
+++ binomial_heap.cpp
@@ -20,7 +20,7 @@ class Node {
             return;
         }
 
-        bool remove_nth_child(int n){
+        bool removeNthChild(int n){
             n-=1;
             if(n<0 || n >= number_of_children)
                 return false;
@@ -35,19 +35,49 @@ class Node {
 
 class Tree {
     private:
-        Node* Head;
-        Node* children[MAX_ORDER];
+        Node* head;
 
         int order;
+
+    public:
+        Tree(Node* node){
+            head = node;
+            order = 0;
+        }
+
+        Node* getHead(){
+            return head;
+        }
+
+        void mergeWith(Tree* new_branch){
+            head->addChild(new_branch->getHead());
+            order +=1;
+        }
 };
 
 class Bheap {
     private:
-        Node* head;
-        Node* next;
-}
+        Tree* head;
+        int binary_rep[MAX_ORDER];
 
+    public:
+        Bheap(Tree* tree){
+            head = tree;
 
+            for(int i=0; i<MAX_ORDER; i++){
+                binary_rep[i] = 0;
+            }
+        }
+        void insert(int new_value){
+            Tree* newtree = new Tree(new Node(new_value));
+
+            while(true){
+                
+            }
+        }
+};
+
+using namespace std;
 int main() {
-
+    cout << 'Hello';
 }
