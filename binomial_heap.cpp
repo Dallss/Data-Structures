25c25d624bb00e4934ee49b8939656e8a2f928e3
Randall Alquicer <randall.bposeats@gmail.com>
2024-12-12 06:19:26 +0800

    adds some functions for Node class

--- binomial_heap.cpp
+++ binomial_heap.cpp
@@ -4,7 +4,33 @@ constexpr int MAX_ORDER = 16;
 class Node {
     private:
         int value;
+        int  number_of_children;
         Node* children[MAX_ORDER];
+    
+    public:
+        Node(int v) {
+            value = v;
+            number_of_children = 0;
+            Node* children[MAX_ORDER];
+        }
+
+        void addChild(Node* new_child) {
+            children[number_of_children] = new_child;
+            number_of_children += 1;
+            return;
+        }
+
+        bool remove_nth_child(int n){
+            n-=1;
+            if(n<0 || n >= number_of_children)
+                return false;
+            else{
+                while(n<number_of_children-1){
+                    children[n] = children[n+1];
+                }
+            }
+        }
+
 };
 
 class Tree {
